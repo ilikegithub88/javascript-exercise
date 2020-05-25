8f4689206dd7b128ce149756ad2cc6adc4ac2cc4
ilikegithub88 <tenbillionandmemory@gmail.com>
2020-05-25 21:09:29 +0900

    create tictactoe

--- tictactoe.js
+++ tictactoe.js
@@ -1,129 +1,136 @@
-var 바디 = document.body;
-var 테이블 = document.createElement('table');
-var 줄들 = [];
-var 칸들 = [];
-var 턴 = 'X';
-var 결과 = document.createElement('div');
+document.body;
+var table=document.createElement('table');
+var records=[];
+var atts=[];
+var turn='O';
+var result=document.createElement('div');
 
-function 결과체크(몇줄, 몇칸) {
-  // 세칸 다 채워졌나?
-  var 다참 = false;
-  // 가로줄 검사
-  if (
-    칸들[몇줄][0].textContent === 턴 &&
-    칸들[몇줄][1].textContent === 턴 &&
-    칸들[몇줄][2].textContent === 턴
-  ) {
-    다참 = true;
-  }
-  // 세로줄 검사
-  if (
-    칸들[0][몇칸].textContent === 턴 &&
-    칸들[1][몇칸].textContent === 턴 &&
-    칸들[2][몇칸].textContent === 턴
-  ) {
-    다참 = true;
-  }
-  // 대각선 검사
-  if (
-    칸들[0][0].textContent === 턴 &&
-    칸들[1][1].textContent === 턴 &&
-    칸들[2][2].textContent === 턴
-  ) {
-    다참 = true;
-  }
-  if (
-    칸들[0][2].textContent === 턴 &&
-    칸들[1][1].textContent === 턴 &&
-    칸들[2][0].textContent === 턴
-  ) {
-    다참 = true;
-  }
-
-  return 다참;
-}
-
-function 초기화(무승부) { // 초기화
-  if (무승부) {
-    결과.textContent = '무승부';
-  } else { // 승부
-    결과.textContent = 턴 + '님이 승리!';
-  }
-
-  setTimeout(function() {
-    결과.textContent = '';
-    칸들.forEach(function (줄) {
-      줄.forEach(function (칸) {
-        칸.textContent = '';
-      });
-    });
-    턴 = 'X';
-  }, 1000);
-}
-
-var 비동기콜백 = function(이벤트) { // 칸을 클릭했을 때
-  if (턴 === 'O') { // 컴퓨터의 턴일 때 내가 클릭하지 않도록
-    return;
-  }
-  var 몇줄 = 줄들.indexOf(이벤트.target.parentNode);
-  var 몇칸 = 칸들[몇줄].indexOf(이벤트.target);
-
-  // 칸이 이미 채워져 있는가?
-  if (칸들[몇줄][몇칸].textContent !== '') { 
-    console.log('빈 칸아닙니다.');
-  } else { // 빈 칸이면
-    console.log('빈 칸입니다');
-    칸들[몇줄][몇칸].textContent = 턴;
-    var 승리여부 = 결과체크(몇줄, 몇칸);
-    // 모든 칸이 다 찼는지 검사
-    var 후보칸 = [];
-    칸들.forEach(function (줄) {
-      줄.forEach(function (칸) {
-        후보칸.push(칸);
-      });
-    });
-    후보칸 = 후보칸.filter(function (칸) { return !칸.textContent }); 
-    if (승리여부) { // 이겼을 경우
-      초기화();
-    } else if (후보칸.length === 0) { // 칸을 더이상 선택할 수 없음
-      초기화(true);
-    } else { // 다 안 찼으면
-      if (턴 === 'X') {
-        턴 = 'O';
-      }
-      setTimeout(function() {
-        console.log('컴퓨터의 턴입니다.');
-        // 빈 칸 중 하나를 고른다.
-        var 선택칸 = 후보칸[Math.floor(Math.random() * 후보칸.length)];
-        선택칸.textContent = 턴;
-        // 컴퓨터가 승리했는지 체크
-        var 몇줄 = 줄들.indexOf(선택칸.parentNode);
-        var 몇칸 = 칸들[몇줄].indexOf(선택칸);
-        var 승리여부 = 결과체크(몇줄, 몇칸);
-        // 다 찼으면
-        if (승리여부) { // 컴퓨터가 이겼을 경우
-          초기화();
-        }
-        // 턴을 나한테 넘긴다.
-        턴 = 'X';
-      }, 1000);
+function check(whatrecord, whatatt){
+    //Is the three-att-row completed?
+    var done=false;
+    //width check
+    if(
+        atts[whatrecord][0].textContent===turn &&
+        atts[whatrecord][1].textContent===turn &&
+        atts[whatrecord][2].textContent===turn
+    ){
+        done=true;
     }
-  }
+    // length check
+    if(
+        atts[0][whatatt].textContent===turn &&
+        atts[1][whatatt].textContent===turn &&
+        atts[2][whatatt].textContent===turn
+    ){
+        done=true;
+    }
+
+    // / check
+    if(
+        atts[0][0].textContent===turn &&
+        atts[1][1].textContent===turn &&
+        atts[2][2].textContent===turn
+    ){
+        done=true;
+    }
+    
+    if(atts[0][2].textContent===turn &&
+        atts[1][1].textContent===turn &&
+        atts[2][0].textContent===turn){
+            done=true;
+        }
+    return done;
+}
+
+function init(draw){
+    if(draw){
+        result.textContent='draw';
+    }else {
+        result.textContent=turn+' wins!';
+    }
+
+    setTimeout(function(){
+        result.textContent='';
+        atts.forEach(function(record){
+            record.forEach(function (att){
+                att.textContent='';
+            });
+        });
+        turn='O';
+    },2000);
+}
+
+//When it's a computer's turn, I shouldn't click one of atts
+var async = function(event){
+    if(turn==='X'){
+        return;
+    }
+
+    var whatrecord=records.indexOf(event.target.parentNode);
+    var whatatt=atts[whatrecord].indexOf(event.target);
+
+    if(atts[whatrecord][whatatt].textContent !==''){
+        console.log('it has been occupied');
+    }else{ 
+        console.log('it is empty');
+        atts[whatrecord][whatatt].textContent=turn;
+        var whether=check(whatrecord,whatatt);
+        
+        var candidate=[];
+        atts.forEach(function(record){
+            record.forEach(function (att){
+                candidate.push(att);
+            });
+        });
+
+        candidate=candidate.filter(function(att){
+            return !att.textContent
+        });
+
+
+        if(whether){
+            init();
+        }else if(candidate.length===0){
+            init(true);
+        }else{
+            if(turn==='O'){
+                turn='X';
+            }
+            setTimeout(function(){
+                var selectedatt=candidate[Math.floor(Math.random()*
+                    candidate.length)];
+                selectedatt.textContent=turn;
+                //whether computer wins or not
+                var whatrecord=records.indexOf(selectedatt.parentNode);
+                var whatatt=atts[whatrecord].indexOf(selectedatt);
+                var whether=check(whatrecord,whatatt);
+                //when every att is full
+                if(whether){
+                    init();
+                }
+                //give me the next turn
+                turn='O';
+            },1000);
+        }
+    }
 };
 
-for (var i = 1; i <= 3; i += 1) {
-  var 줄 = document.createElement('tr');
-  줄들.push(줄);
-  칸들.push([]);
-  for (var j = 1; j <= 3; j += 1) {
-    var 칸 = document.createElement('td');
-    칸.addEventListener('click', 비동기콜백);
-    칸.style="align:center"
-    
-    칸들[i - 1].push(칸);
-    줄.appendChild(칸);
-  }
-  테이블.appendChild(줄);
+//create a table
+for(var i=1; i<=3; i+=1){
+    var record=document.createElement('tr');
+    records.push(record);
+    atts.push([]);
+
+    for(var j=1; j<=3; j+=1){
+        var att=document.createElement('td');
+        att.addEventListener('click',async);
+        att.style="align:center"
+
+        atts[i-1].push(att);
+        record.appendChild(att);
+    }
+    table.appendChild(record);
 }
-바디.appendChild(테이블);
-바디.appendChild(결과);
+document.body.appendChild(table);
+document.body.appendChild(result);
+
